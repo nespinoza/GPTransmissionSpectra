468875298e5bea984075ea909dcc6ba5a70a64e0
Nestor Espinoza <nsespino@uc.cl>
2018-08-11 14:14:18 +0200

    Fixed some bugs on transpec compilation.

--- compile_transpec.py
+++ compile_transpec.py
@@ -13,7 +13,7 @@ args = parser.parse_args()
 ofile = args.ofile
 
 # Read input file:
-datafile, ld_law, all_comps, P, Psd, \
+datafile, ld_law, idx_time, all_comps, P, Psd, \
 a, asd, pmean, psd, b, bsd, t0,\
 t0sd, fixed_eccentricity, ecc, eccsd, \
 omega, omegasd = utils.read_optfile(ofile)
@@ -39,5 +39,8 @@ for wi in wis:
     v,vup,vdown = utils.get_quantiles(p)
     vd,vupd,vdownd = utils.get_quantiles((p**2)*1e6)
     vld,vupld,vdownld = utils.get_quantiles(out['q1'])
-    fout.write('{0:.2f} \t {1:.2f} \t {2:.10f} \t {3:.10f} \t {4:.10f} \t {5:.10f} \t {6:.10f} \t {7:.10f} \t {8:.10f} \t {9:.10f} \t {10:.10f} \n'.format(wbins[0],wbins[1],v,vup-v,v-vdown,vd,vupd-vd,vd-vdownd,vld,vupld-vld,vld-vdownld))
+    try:
+        fout.write('{0:.2f} \t {1:.2f} \t {2:.10f} \t {3:.10f} \t {4:.10f} \t {5:.10f} \t {6:.10f} \t {7:.10f} \t {8:.10f} \t {9:.10f} \t {10:.10f} \n'.format(wbins[0],wbins[1],v,vup-v,v-vdown,vd,vupd-vd,vd-vdownd,vld,vupld-vld,vld-vdownld))
+    except:
+        fout.write('{0:d} \t {1:d} \t {2:.10f} \t {3:.10f} \t {4:.10f} \t {5:.10f} \t {6:.10f} \t {7:.10f} \t {8:.10f} \t {9:.10f} \t {10:.10f} \n'.format(wbins,wbins,v,vup-v,v-vdown,vd,vupd-vd,vd-vdownd,vld,vupld-vld,vld-vdownld))
 fout.close()
