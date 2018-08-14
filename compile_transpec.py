c7a4bee1217ff200689cc5ee46cb6392a8c81c35
Nestor Espinoza <nsespino@uc.cl>
2018-08-14 17:00:40 +0200

    Now outputs include q2 for wavelength dependant fits.

--- compile_transpec.py
+++ compile_transpec.py
@@ -33,14 +33,24 @@ wis = wis[np.argsort(wis)]
 wis = wis.astype(int)
 for wi in wis:
     print 'Working on wbin ',wi,'...'
-    out = pickle.load(open(out_folder+'/wbin'+str(wi)+'/BMA_posteriors.pkl','rb'))
-    wbins = out['wbin']
-    p = out['p']
-    v,vup,vdown = utils.get_quantiles(p)
-    vd,vupd,vdownd = utils.get_quantiles((p**2)*1e6)
-    vld,vupld,vdownld = utils.get_quantiles(out['q1'])
     try:
-        fout.write('{0:.2f} \t {1:.2f} \t {2:.10f} \t {3:.10f} \t {4:.10f} \t {5:.10f} \t {6:.10f} \t {7:.10f} \t {8:.10f} \t {9:.10f} \t {10:.10f} \n'.format(wbins[0],wbins[1],v,vup-v,v-vdown,vd,vupd-vd,vd-vdownd,vld,vupld-vld,vld-vdownld))
+        out = pickle.load(open(out_folder+'/wbin'+str(wi)+'/BMA_posteriors.pkl','rb'))
+        wbins = out['wbin']
+        p = out['p']
+        v,vup,vdown = utils.get_quantiles(p)
+        vd,vupd,vdownd = utils.get_quantiles((p**2)*1e6)
+        vld,vupld,vdownld = utils.get_quantiles(out['q1'])
+        if ld_law == 'linear':
+            try:
+                fout.write('{0:.2f} \t {1:.2f} \t {2:.10f} \t {3:.10f} \t {4:.10f} \t {5:.10f} \t {6:.10f} \t {7:.10f} \t {8:.10f} \t {9:.10f} \t {10:.10f} \n'.format(wbins[0],wbins[1],v,vup-v,v-vdown,vd,vupd-vd,vd-vdownd,vld,vupld-vld,vld-vdownld))
+            except:
+                fout.write('{0:d} \t {1:d} \t {2:.10f} \t {3:.10f} \t {4:.10f} \t {5:.10f} \t {6:.10f} \t {7:.10f} \t {8:.10f} \t {9:.10f} \t {10:.10f} \n'.format(wbins,wbins,v,vup-v,v-vdown,vd,vupd-vd,vd-vdownd,vld,vupld-vld,vld-vdownld))
+        else:
+            vld2,vupld2,vdownld2 = utils.get_quantiles(out['q2'])
+            try:
+                fout.write('{0:.2f} \t {1:.2f} \t {2:.10f} \t {3:.10f} \t {4:.10f} \t {5:.10f} \t {6:.10f} \t {7:.10f} \t {8:.10f} \t {9:.10f} \t {10:.10f} \t {11:.10f} \t {12:.10f} \t {13:.10f} \n'.format(wbins[0],wbins[1],v,vup-v,v-vdown,vd,vupd-vd,vd-vdownd,vld,vupld-vld,vld-vdownld,vld2,vupld2-vld2,vld2-vdownld2))
+            except:
+                fout.write('{0:d} \t {1:d} \t {2:.10f} \t {3:.10f} \t {4:.10f} \t {5:.10f} \t {6:.10f} \t {7:.10f} \t {8:.10f} \t {9:.10f} \t {10:.10f} \t {11:.10f} \t {12:.10f} \t {13:.10f}\n'.format(wbins,wbins,v,vup-v,v-vdown,vd,vupd-vd,vd-vdownd,vld,vupld-vld,vld-vdownld,vld2,vupld2-vld2,vld2-vdownld2))
     except:
-        fout.write('{0:d} \t {1:d} \t {2:.10f} \t {3:.10f} \t {4:.10f} \t {5:.10f} \t {6:.10f} \t {7:.10f} \t {8:.10f} \t {9:.10f} \t {10:.10f} \n'.format(wbins,wbins,v,vup-v,v-vdown,vd,vupd-vd,vd-vdownd,vld,vupld-vld,vld-vdownld))
+        print 'No data (yet)'
 fout.close()
